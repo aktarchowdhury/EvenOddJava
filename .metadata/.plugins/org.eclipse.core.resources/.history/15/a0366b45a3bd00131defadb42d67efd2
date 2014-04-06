b16b157d6226bda58b642e6e615d625653ba091d
aktar chowdhury <aktarchowdhury12@gmail.com>
2014-04-06 12:47:40 -0400

    java ForLoop

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/15/a0366b45a3bd00131defadb42d67efd2
@@ -0,0 +1,13 @@
+package com.aktar.even;
+
+public class ForLoop {
+
+	public static void main(String[] args){
+		for(int z=2; z<90; z++){
+			System.out.println(z+"paul is good");
+			
+		}
+
+	}
+
+}
---
+++ JavaEvenOdd/src/com/aktar/even/While.java
@@ -0,0 +1,13 @@
+package com.aktar.even;
+import java.util.Scanner;
+
+public class While {
+
+	public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	System.out.println("enter number");
+	int a= scan.nextInt();
+
+	}
+
+}
