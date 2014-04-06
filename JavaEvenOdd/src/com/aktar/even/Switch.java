8c7aab347ebf74bb67868df453c51f3c4a8ea198
aktar chowdhury <aktarchowdhury12@gmail.com>
2014-04-06 00:37:13 -0400

    Even& Odd In Java

---
+++ JavaEvenOdd/src/com/aktar/even/Switch.java
@@ -0,0 +1,25 @@
+package com.aktar.even;
+
+public class Switch {
+
+	public static void main(String[] args) {
+	int age;
+	age=4;
+	switch(age){
+	case 1:
+		System.out.println("Can smile");
+	break;
+	case 2:
+		System.out.println("Can crawl");
+		break;
+	case 3:
+		System.out.println("Can walk");
+		break;
+		default:
+			System.out.println("I have no idea");
+			break;
+	}
+
+	}
+
+}
