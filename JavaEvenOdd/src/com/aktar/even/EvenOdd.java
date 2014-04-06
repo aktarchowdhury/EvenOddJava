2f3caae7f361ebfb5e77f962de0b6d7dd37d96ee
aktar chowdhury <aktarchowdhury12@gmail.com>
2014-04-05 23:26:26 -0400

    Even& odd If& Else

---
+++ JavaEvenOdd/src/com/aktar/even/EvenOdd.java
@@ -0,0 +1,17 @@
+package com.aktar.even;
+
+public class EvenOdd extends EvenOdd1 {
+
+	public static void main(String[] args) {
+		Boys();
+	 int v=10,c;
+	c=v%2;
+	if(v==0){
+		System.out.println("Its an even number");
+	}else{
+		System.out.println("Its an odd number");
+	}
+
+	}
+
+}
